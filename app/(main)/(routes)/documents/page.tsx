9ac9bedfe1f3f2bd949a94f0bfe641af9a5fda5b
vivek3454 <vivekparde2411@gmail.com>
2025-04-13 14:00:19 +0530

    Build user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 
 const DocumentsPage = () => {
-  return (
-    <div>Documents</div>
-  )
-}
+  const { user } = useUser();
 
-export default DocumentsPage
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        className="block dark:hidden"
+        src="/file.svg"
+        width="300"
+        height="300"
+        alt="add-document"
+      />
+      <Image
+        className="hidden dark:block"
+        src="/file-dark.svg"
+        width="300"
+        height="300"
+        alt="add-document"
+      />
+
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s DevBoard
+      </h2>
+
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
