b29a769998bbb3823b181bea3445146d84f995ff
vivek3454 <vivekparde2411@gmail.com>
2025-04-19 11:26:03 +0530

    Toolbar component (icon, title)

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,41 @@
+"use client";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
 const DocumentIdPage = () => {
-  return <div>Document Id</div>;
+  const params = useParams();
+  const documentId = params.documentId as Id<"documents">;
+
+  const document = useQuery(api.documents.getById, {
+    documentId: documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
